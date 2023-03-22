5aab1d41436eeb300a1c07f0ce5d606d07174935
Backglass <cocok29@gmail.com>
2023-03-22 11:09:41 +0100

    Añadida función para configurar rutas estáticas que sirven los archivos de static/

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Archivo principal del proyecto
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles #* Este módulo permite servir archivos estáticos como imágenes, CSS, JavaScript, etc. asincronamente.
 from core.config import settings
 
 """ Este código importa el objeto general_pages_router del módulo route_homepage que se encuentra en la carpeta 
@@ -15,15 +16,21 @@ app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
 def include_router(app):
     #* Básicamente, lo que esta función hace es agregar un conjunto predeterminado de rutas y controladores al
     #* objeto app para que pueda ser utilizado por un servidor web.
-    
     app.include_router(general_pages_router)
-    
+
+def configure_static(app):
+    #* Este código configura el servidor para que sirva archivos estáticos (como imágenes, hojas de estilo y archivos de JavaScript)
+    #* en una ruta determinada en lugar de siempre devolver una respuesta HTTP al procesar una petición.
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 def start_application():
     #* Esta función inicializa fastapi con la configuración de la aplicación importada desde settings.py
     #* y usa la funcion include_router para agregar las rutas predeterminadas a la aplicación.
     app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
     include_router(app)
+    configure_static(app)
+    
     return app
 
 app = start_application()
