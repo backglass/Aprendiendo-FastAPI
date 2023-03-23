3ce42911454742eadb8c6a24d25c5e9ce9bf9733
Backglass <cocok29@gmail.com>
2023-03-23 14:05:23 +0100

    nada importante

--- core/config.py
+++ core/config.py
@@ -20,9 +20,9 @@ class Settings:
     
     POSTGRES_USER: str = os.getenv("POSTGRES_USER")
     POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
-    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER")
+    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER","localhost")
     POSTGRES_PORT: str = os.getenv("POSTGRES_PORT",5432)
-    POSTGRES_DB: str = os.getenv("POSTGRES_DB")
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB","tdd")
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
        
 settings = Settings() # Importa esta configuración en tu archivo principal
