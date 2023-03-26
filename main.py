ba21b18c7ea027c78b40b6b7b90109f535aeb038
Backglass <cocok29@gmail.com>
2023-03-26 16:42:59 +0200

    Cambiada la routa de donde importamos todas las rutas de la app

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ from core.config import settings
 
 from db.session import engine #engine de db.session, que es una instancia de SQLAlchemy que permite conectar con la base de datos a través de funciones para crear, insertar, modificar y eliminar registros.
 from db.base import Base #Base que a su vez esta en de db.base_class
+from apis.base import api_router  #Importa las Rutas de la API
 
 """ Este código importa el objeto general_pages_router del módulo route_homepage que se encuentra en la carpeta 
 apis.general_pages
 . Presumiblemente, este objeto es una instancia de un enrutador que maneja las rutas relacionadas
 con la página de inicio para una API. """
-from apis.general_pages.route_homepage import general_pages_router #Rutas predeterminadas
+
 
 
 app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
@@ -19,7 +20,7 @@ app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
 def include_router(app):
     #* Básicamente, lo que esta función hace es agregar un conjunto predeterminado de rutas y controladores al
     #* objeto app para que pueda ser utilizado por un servidor web.
-    app.include_router(general_pages_router)
+    app.include_router(api_router)
 
 def configure_static(app):
     #* Este código configura el servidor para que sirva archivos estáticos (como imágenes, hojas de estilo y archivos de JavaScript)
