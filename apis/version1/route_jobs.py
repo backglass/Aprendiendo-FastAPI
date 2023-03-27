30588226cfa069b8d21ca0d10b4f008e1bc12a04
Backglass <cocok29@gmail.com>
2023-03-27 19:26:44 +0200

    Añadida la ruta /get que recibe un entero para buscar un trabajo por su id

--- apis/version1/route_jobs.py
+++ apis/version1/route_jobs.py
@@ -6,6 +6,7 @@ from db.session import get_db
 from db.models.jobs import Job
 from schemas.jobs import JobCreate, ShowJob
 from db.repository.jobs import create_new_job
+from db.repository.jobs import retrieve_job
 """
 Este código define un APIRouter desde la biblioteca FastAPI de Python y define un endpoint
 POST "/create-job". Cuando se llama a este endpoint, se crea una nueva tarea en la base de datos
@@ -15,6 +16,7 @@ para validar los datos de entrada y salida del usuario.
 También se usa la clase Job en "db/models/jobs.py" para leer y escribir datos de la base de datos.
 La clase create_new_job definida en "db/repository/jobs.py" se utiliza para crear un nuevo trabajo
 en la base de datos y, finalmente, se devuelve el trabajo recién creado al usuario.
+La clase retrieve_job definida en "db/repository/jobs.py" se utiliza para recuperar un trabajo
 """
 
 
@@ -26,4 +28,16 @@ router = APIRouter()
 def create_job(job: JobCreate, db: Session = Depends(get_db)):
     current_user = 1
     job = create_new_job(job=job, db=db, owner_id=current_user)
+    return job
+
+
+# Crear una ruta GET que recibe un id y retorna un trabajo
+@router.get("/get/{id}", response_model=ShowJob)
+def read_job(id: int, db: Session = Depends(get_db)):
+    # Llamar a la función que buscará el trabajo en la base de datos
+    job = retrieve_job(id=id, db=db)
+    # Si el trabajo no existe, levantar una excepción 404
+    if not job:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Job with the id {id} is not available")
+    # Si el trabajo existe, retornarlo
     return job
