72d4e1c357deaf20c8e67aaac7b3788146a96b7a
Backglass <cocok29@gmail.com>
2023-03-31 16:12:06 +0200

    Añadidos datos para configurar los token

--- core/config.py
+++ core/config.py
@@ -24,6 +24,11 @@ class Settings:
     POSTGRES_PORT: str = os.getenv("POSTGRES_PORT",5432)
     POSTGRES_DB: str = os.getenv("POSTGRES_DB","tdd")
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    
+    #Datos para token JWT
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
        
 settings = Settings() # Importa esta configuración en tu archivo principal
 
