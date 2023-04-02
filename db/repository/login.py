a3963773aed69e43f3499582493858427cb02e3d
Backglass <cocok29@gmail.com>
2023-04-02 16:54:22 +0200

    Corregido para que verifique el email con el username

--- db/repository/login.py
+++ db/repository/login.py
@@ -4,17 +4,23 @@ from db.models.users import User
 
 
 def get_user(username: str, db: Session):
-    user = db.query(User).filter(User.username == username).first()
+    user = db.query(User).filter(User.email == username).first()
     return user
 
-""" Este código define una función llamada "get_user" que toma dos parámetros de entrada: "username"
-que es el nombre de usuario y "db" que es un objeto de sesión que representa una conexión a la base
-de datos.
+"""  
+Este código define una función llamada "get_user". Esta función recibe dos argumentos:
+"username" que es un objeto de tipo string que representa el email del usuario que se 
+desea obtener, y "db" que es una sesión de base de datos SQLAlchemy. 
 
-La función utiliza el objeto "db" para acceder a la base de datos y buscar un objeto "User" cuyo nombre
-de usuario es igual al valor de "username". 
+Luego, dentro de la función, se realiza una consulta a la base de datos utilizando el método
+"query" de la sesión pasada como argumento. El ".filter" se encarga de buscar en la tabla
+"User" aquellos registros cuyo campo email sea igual al valor de "username". Finalmente, el
+método "first" devuelve el primer registro encontrado. 
 
-Luego, la función devuelve el primer objeto "User" que coincide con la consulta.  """
+La función retorna una instancia de la entidad User con los valores correspondientes a la fila
+encontrada, o retorna "None" si no se encontró un registro que coincida con la consulta
+realizada.
+"""
 
 #* En resumen, la función "get_user" obtiene un objeto "User" de la base de datos mediante el nombre de
 #* usuario proporcionado.
