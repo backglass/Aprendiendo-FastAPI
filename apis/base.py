006df555e2d8a7300a3004606c7cde74fa7d82af
Backglass <cocok29@gmail.com>
2023-04-21 12:15:02 +0200

    Se elimina la routa / que pertenece ahora a el archivo base.py de webapps

--- apis/base.py
+++ apis/base.py
@@ -19,7 +19,7 @@ api_router = APIRouter()
 
 
 api_router = APIRouter()
-api_router.include_router(route_general_pages.general_pages_router,prefix="",tags=["general_pages"])
+#! Eliminado api_router.include_router(route_general_pages.general_pages_router,prefix="",tags=["general_pages"])
 api_router.include_router(route_users.router,prefix="/users",tags=["users"])
 api_router.include_router(route_jobs.router,prefix="/jobs",tags=["jobs"])
 api_router.include_router(route_login.router,prefix="/login",tags=["login"])
