ac48a1ed34d7a5423d33cfef9b6c9e5cc31351c6
Backglass <cocok29@gmail.com>
2023-04-23 11:49:44 +0200

    Añadida ruta detalles para un trabajo

--- webapps/jobs/route_jobs.py
+++ webapps/jobs/route_jobs.py
@@ -4,35 +4,40 @@ from fastapi import Request, Depends
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 
-#* Importamos la función list_jobs de la carpeta jobs del archivo repository y la función get_db de session del archivo db
+
 from db.repository.jobs import list_jobs
 from db.session import get_db
+from db.repository.jobs import retrieve_job
 
 #* Creamos una variable templates que permitirá cargar plantillas HTML
 templates = Jinja2Templates(directory="templates")
 
-#* Creamos una variable router que nos permitirá definir rutas para nuestra API
+
 router = APIRouter(include_in_schema=False)
 
 #* Definimos la ruta principal de la API "/"
 @router.get("/")
 async def home(request: Request, db: Session=Depends(get_db)):
-    #* Llamamos a la función list_jobs para obtener una lista de trabajos desde nuestra base de datos
+
     jobs = list_jobs(db=db)
-    
-    #* Devolvemos una respuesta utilizando una plantilla HTML llamada "homepage.html", junto con una variable "jobs" que contiene la lista de trabajos obtenida anteriormente
+    print ("jobs: ", jobs)
     return templates.TemplateResponse("general_pages/homepage.html", {"request": request, "jobs": jobs})
 
 
+@router.get("/details/{id}")
+def job_detail(id: int, request: Request, db: Session=Depends(get_db)):
+    job = retrieve_job(id=id, db=db)
+    print("job: ")
+    return templates.TemplateResponse("jobs/detail.html", {"request": request, "job": job})
 
-# Este código crea una ruta en una API de FastAPI, que hace una solicitud GET al directorio raíz.
-# Cuando se accede al directorio raíz, se ejecuta la función home que utiliza la dependencia get_db() para
-# obtener una conexión de base de datos.
+""" 
+La variable templates se usa para cargar plantillas HTML que se mostrarán en la página web.
+La ruta principal de la API es la página de inicio y se define en la ruta "/" usando el método GET.
+Esto devuelve una página HTML personalizada que incluye trabajos listados.
 
-# La función home llama a list_jobs() que es una función de un módulo denominado jobs que se encarga de listar
-# todos los trabajos disponibles.
-# Los trabajos se pasan como un contexto a la plantilla HTML llamada "general_pages/homepage.html", junto
-# con una solicitud request.
 
-# En resumen, lo que hace este código es listar los trabajos disponibles en una página web en el directorio raíz
-# de la API de FastAPI
+Además, existe otra ruta llamada "/details" que se utiliza para mostrar detalles de un trabajo en
+particular, siendo id el identificador único de cada trabajo. Cuando el usuario navega a la página
+"/details/{id}", se muestra una plantilla HTML personalizada con detalles de ese trabajo. """
+
+
