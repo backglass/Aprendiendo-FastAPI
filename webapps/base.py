4767ef3a52dca1542a8290c079643107a2b3751d
Backglass <cocok29@gmail.com>
2023-04-25 17:10:27 +0200

    Añadida ruta para el login

--- webapps/base.py
+++ webapps/base.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from webapps.jobs import route_jobs
 from webapps.users import route_users
+from webapps.auth import route_login
 
 """ Este código importa la función route_jobs del módulo webapps.jobs y la asocia con una instancia de la clase
 APIRouter de FastAPI para crear y configurar rutas de la API web. Luego, se incluye esta ruta dentro de otra
@@ -11,3 +12,4 @@ que se utilizará para organizar y documentar la API. """
 api_router = APIRouter()
 api_router.include_router(route_jobs.router, prefix="", tags=["job-webapp"])
 api_router.include_router(route_users.router, prefix="", tags=["user-webapp"])
+api_router.include_router(route_login.router, prefix="", tags=["auth-webapp"])
